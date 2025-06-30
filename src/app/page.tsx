f21029a354b43cfc0015aa7d0c2c9814274ea94e
Diptech-hub <oladipupobabayemi@gmail.com>
2025-06-30 21:48:02 +0100

    loading update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,17 @@ import { useState } from "react";
 
 export default function Home() {
   const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     if (username) {
+      setLoading(true);
       router.push(`/${username}`);
     }
   };
+
 
   return (
     <div className="flex flex-col items-center px-6 md:px-12 lg:px-20 text-center">
@@ -38,7 +41,7 @@ export default function Home() {
           className="bg-white text-black px-4 py-2 rounded-2xl text-sm mt-2 md:mt-0 md:ml-4"
           type="submit"
         >
-          twitter
+         {loading ? "Loading..." : "twitter"}
         </button>
       </form>
       <div className="grid grid-cols-2 md:flex md:flex-row text-white text-sm gap-4 mt-6">
